80ce2ed0b28439c4f917fa3cc10c44dedbbd0fe8
x14g019 <x14g019@PC504N011.chiba-fjb.ac.jp>
2016-01-19 12:37:09 +0900

    データ受け取り処理の記入をする

--- src/DB/table.java
+++ src/DB/table.java
@@ -2,6 +2,7 @@ package DB;
 
 import java.io.IOException;
 import java.io.PrintWriter;
+import java.sql.Date;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.util.ArrayList;
@@ -14,110 +15,126 @@ import javax.servlet.http.HttpServletResponse;
 
 import net.arnx.jsonic.JSON;
 
-class RecvData
-{
-	public String cmd;
-	public String name;
-	public String msg;
+class RecvData {
+	public String Kategori;
+	public String Gaiyou;
+	public int Kingaku;
+	public int cmd;
 }
 
-class SendData
-{
-	public int id;
-	public String name;
-	public String msg;
+class SendData {
+	public String Kategori;
+	public String Gaiyou;
+	public int Kingaku;
+	public int cmd;
 }
 
 /**
  * Servlet implementation class Ajax10
  */
-@WebServlet("/Ajax10")
+@WebServlet("/table")
 public class table extends HttpServlet {
 	private static final long serialVersionUID = 1L;
-	private DB.table mOracle;
-	private final String DB_ID = "x14g009";
-	private final String DB_PASS = "a";
+	private DB.Oracle mOracle;
+	private final String DB_ID = "x14g019";
+	private final String DB_PASS = "sakura39";
 
-    /**
-     * @see HttpServlet#HttpServlet()
-     */
-    public table() {
-        super();
-        // TODO Auto-generated constructor stub
-    }
+	/**
+	 * @see HttpServlet#HttpServlet()
+	 */
+	public table() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+
 	public void init() throws ServletException {
 		// TODO 自動生成されたメソッド・スタブ
 		super.init();
 
-
-		try{
+		try {
 			mOracle = new Oracle();
 			mOracle.connect("ux4", DB_ID, DB_PASS);
 
-			//テーブルが無ければ作成
-			if(!mOracle.isTable("db_exam10"))
-			{
-				mOracle.execute("create table db_exam10(id number,name varchar2(200),msg varchar2(200))");
-				mOracle.execute("create sequence db_exam10_seq");
-			
+			// テーブルが無ければ作成
+			if (!mOracle.isTable("main_table")) {
+				mOracle.execute("create table main_table(レコード番号 int,ユーザID varchar2(10),日付 date,カテゴリ int,概要 varchar2(80),金額 int)");
+				mOracle.execute("create sequence main_table_SeqID");
+			}
+			if (!mOracle.isTable("Kategori_table")) {
+				mOracle.execute("create table Kategori_table(カテゴリID　int,ユーザID varchar2(10),カテゴリ名  varchar2(20))");
+				mOracle.execute("create sequence Kategori_table_SeqID");
+			}
+			System.out.println("テーブル作成完了");
 		} catch (Exception e) {
 			System.err.println("認証に失敗しました");
 		}
 	}
-
+	void insertData(String user,Date hiduke, int kategori, String gaiyou, int kingaku){
+		
+	}
 	@Override
 	public void destroy() {
-		//DB切断
+		// DB切断
 		mOracle.close();
 		// TODO 自動生成されたメソッド・スタブ
 		super.destroy();
 	}
+
 	/**
-	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse
+	 *      response)
 	 */
-	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		action(request,response);
+	protected void doGet(HttpServletRequest request,
+			HttpServletResponse response) throws ServletException, IOException {
+		action(request, response);
 	}
 
 	/**
-	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse
+	 *      response)
 	 */
-	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		action(request,response);
+	protected void doPost(HttpServletRequest request,
+			HttpServletResponse response) throws ServletException, IOException {
+		action(request, response);
 	}
-	private void action(HttpServletRequest request, HttpServletResponse response) throws IOException {
-		//出力ストリームの作成
-        response.setCharacterEncoding("UTF-8");
-        response.setContentType("text/plain; charset=UTF-8");
-        PrintWriter out = response.getWriter();
 
-        //データの受け取り処理
-        RecvData recvData = JSON.decode(request.getInputStream(),RecvData.class);
-        if("write".equals(recvData.cmd))
-        {
-        	//書き込み処理
-        	String sql = String.format("insert into db_exam10 values(db_exam10_seq.nextval,'%s','%s')",
-        			recvData.name,recvData.msg);
-        	mOracle.execute(sql);
-        }
+	private void action(HttpServletRequest request, HttpServletResponse response)
+			throws IOException {
+		// 出力ストリームの作成
+		response.setCharacterEncoding("UTF-8");
+		response.setContentType("text/plain; charset=UTF-8");
+		PrintWriter out = response.getWriter();
 
+		// データの受け取り処理
+		RecvData recvData = JSON.decode(request.getInputStream(),
+				RecvData.class);
+		if ("write".equals(recvData.cmd)) {
+			// 書き込み処理
+			System.out.println("データ書き込み");
+			String sql = String
+					.format("insert into main_table values(main_table_seq.nextval,'%s','%s')",
+							recvData.Kategori, recvData.Gaiyou,
+							recvData.Kingaku);
+			mOracle.execute(sql);
+		}
 
-        try {
-			//データの送信処理
+		try {
+			// データの送信処理
 			ArrayList<SendData> list = new ArrayList<SendData>();
-			ResultSet res = mOracle.query("select * from db_exam10 order by id");
-			while(res.next())
-			{
+			ResultSet res = mOracle
+					.query("select * from main_table order by id");
+			while (res.next()) {
 				SendData sendData = new SendData();
-				sendData.id = res.getInt(1);
-				sendData.name = res.getString(2);
-				sendData.msg = res.getString(3);
+				sendData.Kategori = res.getString(1);
+				sendData.Gaiyou = res.getString(2);
+				sendData.Kingaku = res.getInt(3);
+				sendData.cmd = res.getInt(4);
 				list.add(sendData);
 			}
-			//JSON形式に変換
-            String json = JSON.encode(list);
-            //出力
-            out.println(json);
+			// JSON形式に変換
+			String json = JSON.encode(list);
+			// 出力
+			out.println(json);
 		} catch (SQLException e) {
 			e.printStackTrace();
 		}
